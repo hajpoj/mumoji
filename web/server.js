a916765064371cec7d4d1c752fb9992707139b86
Anshuman Srivastava <anshuman.srivastava@gmail.com>
2014-03-14 16:11:21 -0700

    added youtube video as response
    added both embedable code for youtube as well as url for a specific
    route

--- web/server.js
+++ web/server.js
@@ -5,7 +5,11 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', function(req, res) {
-  res.send('hello world');
+  res.send('<iframe width="560" height="315" src="//www.youtube.com/embed/9bZkp7q19f0?list=PLirAqAtl_h2r5g8xGajEwdXd3x1sZh8hC" frameborder="0" allowfullscreen></iframe>');
+});
+
+app.get('/estimote', function(req, res) {
+  res.send('http://www.youtube.com/watch?v=9bZkp7q19f0&feature=share&list=PLirAqAtl_h2r5g8xGajEwdXd3x1sZh8hC');
 });
 
 var server = http.createServer(app);
